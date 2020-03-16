144760b53ba7c3b5bda4d0c3a08eb8e54402c9d6
mahamed-gamal <mohammedgamal319mg@gmail.com>
2020-03-16 17:47:45 +0200

    Bulding the goal cell

---
+++ goalpost-app/View/GoalCell.swift
@@ -0,0 +1,22 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Mohamed Gamal on 3/16/20.
+//  Copyright © 2020 ME. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    
+    func configureCell(description: String , type: String , goalProgressAmount: Int) {
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+}
