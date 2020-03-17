bf4feef2a2c8b6c3852a06a09c013d044fc0db59
mahamed-gamal <mohammedgamal319mg@gmail.com>
2020-03-17 13:50:09 +0200

    creating static goal cell

--- goalpost-app/Controller/GoalsVc.swift
+++ goalpost-app/Controller/GoalsVc.swift
@@ -16,11 +16,31 @@ class GoalsVc: UIViewController {
     //MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     //MARK: - Actions
     @IBAction func createGoalDidPressed(_ sender: Any) {
         print("button is pressed...")
+    }
+}
+
+extension GoalsVc: UITableViewDelegate , UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GoalCell") as? GoalCell else {return UITableViewCell()}
+        
+        cell.configureCell(description: "Eat salad twice a weak.", type: .shortTerm, goalProgressAmount: 2)
+        
+        return cell
     }
 }
 
---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,14 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Mohamed Gamal on 3/17/20.
+//  Copyright © 2020 ME. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case shortTerm = "Short Term"
+    case longTerm = "Long Term"
+}
