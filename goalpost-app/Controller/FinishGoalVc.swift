33704a8c7fbd9b8aceedeae5612d3d861fd25f0a
mahamed-gamal <mohammedgamal319mg@gmail.com>
2020-03-22 18:38:46 +0200

    done saving in core data

--- goalpost-app/Controller/FinishGoalVc.swift
+++ goalpost-app/Controller/FinishGoalVc.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVc: UIViewController , UITextFieldDelegate {
     
@@ -31,7 +32,35 @@ class FinishGoalVc: UIViewController , UITextFieldDelegate {
     
     //MARK: - Actions
     @IBAction func createGoalBtnPressed(_ sender: UIButton) {
+        if pointsTextField.text != "" {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismissDetail()
+    }
+    
+    func save(completion: (_ finished: Bool) -> ()) {
+        guard let managedContext = appdelegate?.persistentContainer.viewContext else {return}
+        let goal = Goal(context: managedContext)
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalProgress = Int32(0)
+        goal.goalCompletedValue = Int32(pointsTextField.text!)!
         
+        do {
+            try managedContext.save()
+            print("succesfully saved data...")
+            completion(true)
+        } catch {
+            print("could not save \(error.localizedDescription)")
+            completion(false)
+        }
     }
     
 }
--- goalpost-app/Controller/GoalsVc.swift
+++ goalpost-app/Controller/GoalsVc.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import CoreData
+
+let appdelegate = UIApplication.shared.delegate as? AppDelegate
 
 class GoalsVc: UIViewController {
     
